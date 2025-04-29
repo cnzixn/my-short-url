034e2b29a186aaf5d0519b672163168ed17b137c
cnzixn <cnzixn@gmail.com>
2025-04-29 10:08:23 +0800

    Update main at 2025-04-29 10:08:23

--- public/script.js
+++ public/script.js
@@ -128,24 +128,3 @@ async function deleteLink(key) {
     }
 }
 
-
-
-// 获取链接和二维码
-async function getLinkData(key) {
-    try {
-        const response = await fetch('/.netlify/functions/getLink', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ key })
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        return await response.json();
-    } catch (error) {
-        console.error('API调用失败:', error);
-        throw error;
-    }
-};
